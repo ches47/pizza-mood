53eeda22f0434f237647fc1e353b9de4304f6759
Ches <chesy47@gmail.com>
2022-08-11 09:38:37 +0300

    Pizza-Mood part 4 --start

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -5,7 +5,7 @@ function Head() {
       <div className="header">
         <div className="container">
           <div className="header__logo">
-            <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo" />
+            <img width="38" src="img/pizza-logo.svg" alt="Pizza logo" />
             <div>
               <h1>Pizza Mood</h1>
               <p>самая вкусная пицца во вселенной</p>
