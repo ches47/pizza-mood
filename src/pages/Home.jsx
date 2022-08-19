3212dc57f4dce4dd61fae483dc5b061ebcfbc0dc
Ches <chesy47@gmail.com>
2022-08-19 12:58:40 +0300

    Pizza-Mood part 8 --done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 import Category from '../components/Category';
 import Sort from '../components/Sort';
@@ -6,33 +6,33 @@ import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 
 const Home = () => {
-    const [items, setItems] = React.useState([]);
-    const [isLoading, setIsLoading] = React.useState(true);
-  
-    React.useEffect(() => {
-      fetch('https://62f69273a3bce3eed7c40a0e.mockapi.io/Items')
-        .then((res) => res.json())
-        .then((arr) => {
-          setItems(arr);
-          setIsLoading(false);
-        });
-    }, []);
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
-    return(
-        <>
-        <div className="content__top">
-                <Category />
-                <Sort />
-            </div>
-            <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-                {isLoading
-                ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-                : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-        </div>
-        </>
-    )
-    
+  React.useEffect(() => {
+    fetch('https://62f69273a3bce3eed7c40a0e.mockapi.io/Items')
+      .then((res) => res.json())
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Category />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
