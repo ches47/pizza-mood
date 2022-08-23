e0381e2ebb5b7b659b630b7c1bf69e7138d5ba9e
Ches <chesy47@gmail.com>
2022-08-23 09:24:56 +0300

    Pizza-Mood part 11 --done

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Head />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home searchValue={searchValue} />} />
+            <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
