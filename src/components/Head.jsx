f64564adbb3ed86da86c2d8e97eee806a8454848
Ches <chesy47@gmail.com>
2022-08-23 09:14:22 +0300

    Pizza-Mood part 11 --start

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import logoSvg from '../assets/img/pizza-logo.svg'; //как импорт-ь картинку через реакт
 import Search from './Search';
 
-function Head({ searchValue, setSearchValue }) {
+function Head() {
   return (
     <div className="header">
       <div className="container">
@@ -18,7 +18,7 @@ function Head({ searchValue, setSearchValue }) {
             </div>
           </div>
         </Link>
-        <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+        <Search />
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
             <span>520 ₽</span>
