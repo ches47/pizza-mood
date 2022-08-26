b25dfb5eb51e2d9b25687f213c4e28961a62b595
Ches <chesy47@gmail.com>
2022-08-26 15:00:33 +0300

    Pizza-Mood part 13 --done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, test } from './redux/slices/filterSlice';
 import { Routes, Route } from 'react-router-dom';
 
 import Head from './components/Head';
@@ -12,9 +14,18 @@ export const SearchContext = React.createContext();
 
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
 
   return (
     <div className="wrapper">
+      <button aria-label="Increment value" onClick={() => dispatch(test())}>
+        Increment
+      </button>
+      <span>{count}</span>
+      <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
       <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Head />
         <div className="content">
